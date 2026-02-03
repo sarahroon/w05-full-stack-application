ccb96c18cf417db48cd0887a8e6fda76eed3a856
sarahroon <sarahrooney130500@gmail.com>
2026-02-03 11:10:41 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,17 +11,3 @@ dotenv.config();
 const db = new pg.Pool({
   connectionString: process.env.DB_CONN,
 });
-
-const express = require("express");
-const app = express();
-
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.listen(3000, () => {
-  console.log("Running on port 3000");
-});
